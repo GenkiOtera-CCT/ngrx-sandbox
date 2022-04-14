ac41f34038972785c95c08a9019594be3d6212f9
GenkiOtera-CCT <genki.otera@cct-inc.co.jp>
2022-04-14 15:26:09 +0900

    storeのデバッグツール(@ngrx/store-devtools)をインストール
    こちらもangularのバージョンに合わせないとbuildに失敗

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 import { counterReducer } from './store/counter.reducer';
 import { MyCounterComponent } from './components/my-counter/my-counter.component';
 
@@ -15,7 +16,8 @@ import { MyCounterComponent } from './components/my-counter/my-counter.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ count: counterReducer })
+    StoreModule.forRoot({ count: counterReducer }),
+    StoreDevtoolsModule.instrument(),
   ],
   providers: [],
   bootstrap: [AppComponent]
