5ab82fa565b20a6d1e06b157437ce4073a6c54f2
GenkiOtera-CCT <genki.otera@cct-inc.co.jp>
2022-04-15 11:32:58 +0900

    API URLのアダプタを実装

--- src/app/mock-web-api.ts
+++ src/app/mock-web-api.ts
@@ -1,4 +1,5 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api'
+import { entityConfig } from './entity-metadata';
 
 export class MockWebApi implements InMemoryDbService {
     private api = {
@@ -10,5 +11,37 @@ export class MockWebApi implements InMemoryDbService {
 
     createDb(){
         return this.api;
+    }
+
+    urlTranslate(url: string): string{
+        const regexp = /(\/?[^\/]+\/)([^\/]+)(\/?.*)/g;
+        const match = regexp.exec(url);
+    
+        if(!match || match.length !== 4 ) { throw new Error('URL形式が不正です')}
+    
+        const requestEntityName = match[2];
+        const apiEntityName = this.convertApiEntityName(requestEntityName);
+    
+        return url.replace(regexp, `$1${apiEntityName}$3`)
+          .replace(/\/\?/g, '?')
+          .replace(/\/$/g, '');
+    }
+
+    // Private Methods
+    private get lowerCasePluralNames(): {} | { [name: string]: string } {
+        return !entityConfig.pluralNames ? {} :
+            Object.entries(entityConfig.pluralNames)
+            .reduce((x, y) => 
+                Object.assign(x, { [y[0].toLowerCase()]: y[1].toLowerCase() }), {});
+    }
+    
+    private toPlural(singluarName: string): string {
+        return this.lowerCasePluralNames[singluarName] || `${singluarName}s`;
+    }
+
+    private convertApiEntityName(entityName: string): string {
+        return Object.keys(this.api)
+            .map(x => x.toLowerCase())
+            .includes(entityName) ? entityName : this.toPlural(entityName);
     }
 }
